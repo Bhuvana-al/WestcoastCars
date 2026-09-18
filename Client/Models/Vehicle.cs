f51ca5123027ae8182619ed2fef633f32966635a
Bhuvaneswari Adhiseshan <bhuvaneswari.adiseshan@gmail.com>
2026-09-18 11:28:25 +0200

    Added Models

---
+++ Client/Models/Vehicle.cs
@@ -0,0 +1,27 @@
+﻿namespace Client.Models;
+
+public class Vehicle
+{
+    //skapa privata fält for tillverkare och modell
+
+    //privata fälten (backing fields)
+    //Detta är vårt data => information
+    private string _manufacturer;
+    private string _model;
+
+    //Beteende => Logik, manipulera värt data => tillstånd
+    //skapa egenskaper som laser och skriver varden till de privata falten
+    public string Manufacturer
+    { 
+        //Läser värdet på det privata faltet..
+        get {return _manufacturer;}
+        //Skriva ett värdet till det privata faltet..
+        set {_manufacturer = value;}
+    }    
+    public string Model
+    {
+        get {return _model;}
+        set {_model = value;}
+    }
+
+}
--- Client/Program.cs
+++ Client/Program.cs
@@ -1,30 +1,27 @@
-﻿namespace Client;
+﻿using Client.Models;
+namespace Client;
 
 class Program
 {
     //skapa en array som lagrar 3 stranger(tillverkare av bilar)
     static void Main()
     {
-        //string[] vehicles = ["Volvo", "Ford", "Fiat"];
-        List<string> vehicles = ["Volvo", "Ford", "Fiat"];
+        //korrekt syntax for att instansiera ett objekt..
+        //volvo ar ett objekt av typen vehicle...
+        Vehicle volvo = new Vehicle();
 
-        Console.WriteLine(vehicles[1]);
-        // Andra Ford till Volkswagen
-        vehicles[1] = "Volkswagen";
-        Console.WriteLine(vehicles[1]);
-        //for is better than foreach
-        /*for (int i=0; i < vehicles.Length; i++) //string[]
-        {
-            Console.WriteLine(vehicles[i]);
-        } */
-        for (int i=0; i < vehicles.Count; i++) //List
-        {
-            Console.WriteLine(vehicles[i]);
-        }
-        vehicles.Add("Mercedes");
-        foreach(var vehicle in vehicles)
-        {
-            Console.WriteLine(vehicle);
-        }
+        volvo.Manufacturer = "Volvo"; //Anrop sker nu till set i egenskapen
+        volvo.Model = "EX30";
+
+        string model = volvo.Model; //anrop get
+        Console.WriteLine(volvo.Manufacturer); 
+        Console.WriteLine(volvo.Model); 
+
+        Vehicle x = new Vehicle();
+        x.Manufacturer = "Ford";
+        x.Model = "Fusion";
+
+        Console.WriteLine(x.Manufacturer);
+        Console.WriteLine(x.Model);
     }
 }
